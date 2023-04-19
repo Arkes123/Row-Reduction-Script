210ade37bae3e8a46ab1359081508baa61c24f0b
Jon Darius <chon@Jons-MacBook-Pro.local>
2023-04-19 14:26:04 -0700

    Starting python script template (menu)

---
+++ row-alg.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def generateMatrix(numRows, numCols):
+    print("Enter contents of the matrix")
+
+def main():
+    print("---------------------------------------------------------------------------------")
+    print("-------------------Linear Alebra Calculator: Row Reduction ----------------------")
+    print("---------------------------------------------------------------------------------")
+
+    print("\n")
+
+    numRows = int(input("Number of Rows ~ "))
+    print("\n")
+    numCols = int(input("Number of Columns ~ "))
+    
+    print("\n")
+
+    generateMatrix(numRows, numCols)
+
+if __name__ == '__main__':
+    main()
