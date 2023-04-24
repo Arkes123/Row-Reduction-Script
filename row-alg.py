e88f68d60877cdea991b804ee52c79c757bfdffd
Arkes123 <jonathan.darius2015@gmail.com>
2023-04-24 14:42:24 -0700

    Converted to OOP for accesibility

--- row-alg.py
+++ row-alg.py
@@ -1,6 +1,6 @@
-class rowOperations:
+class rowOps:
     def interchange(matrix, i, j):
-        matrix[i], matrix[j] = matrix[j], matrix[i]
+        matrix.userMatrix[i], matrix.userMatrix[j] = matrix.userMatrix[j], matrix.userMatrix[i]
         return matrix
 
 
@@ -11,66 +11,61 @@ class rowOperations:
     def addRowToScaler(matrix, i, j):
         pass
 
-class matrixFunctions:
-    def __init__(self):
-        pass
+    def presentOperations():
+        print("1) Interchange rows")
+        print("2) Scale a row by a real number")
+        print("3) Add a row to a scalar multiple of another")
 
+class base:
+    def __init__(self, userMatrix, numRows, numCols):
+        self.userMatrix = userMatrix
+        self.numRows = numRows
+        self.numCols = numCols
 
+    def generateMatrix(numRows, numCols, userMatrix):
+        for i in range(numRows):
+            rows = []
+            for j in range(numCols):
+                rows.append(int(input("Enter an element: ")))
+            userMatrix.append(rows)
+        
+        matrix = base(userMatrix, numRows, numCols)
+        return matrix
 
-def generateMatrix(numRows, numCols, matrix):
-    count = 0
-    for i in range(numRows):
-        rows = []
-        for j in range(numCols):
-            rows.append(int(input("Enter an element: ")))
-        matrix.append(rows)
+    def printGivenMatrix(matrix):
+        for i in range(matrix.numRows):
+            for j in range(matrix.numCols):
+                print(matrix.userMatrix[i][j], end=" ")
+            print()
+    
+    def regularEchelon(matrix):
+        print("Here is you matrix...")
+        print("\n")
 
-    print("\n")
-    print("Here is your generated matrix...")
-    print("\n")
+        print("What operation would you like to perform? ")
+        rowOps.presentOperations()
 
-    for i in range(numRows):
-        for j in range(numCols):
-            print(matrix[i][j], end=" ")
-        print()
+        try:
+            userOption = int(input("Enter option: "))
+            if userOption == 1:
+                firstRow = int(input("1st row to swap: "))
+                secondRow = int(input("2nd row to swap: "))
+                rowOps.interchange(matrix, firstRow, secondRow)
+                base.printGivenMatrix(matrix)
+            elif userOption == 2:
+                pass
+            elif userOption == 3:
+                pass
+            else:
+                raise ValueError
+        except ValueError:
+            print("Invalid option!")
 
-def presentOperations():
-    print("1) Interchange rows")
-    print("2) Scale a row by a real number")
-    print("3) Add a row to a scalar multiple of another")
-
-def regularEchelon(matrix):
-    print("Here is you matrix...")
-    print("\n")
-
-    print("What operation would you like to perform? ")
-    presentOperations()
-
-    try:
-        userOption = int(input("Enter option: "))
-        if userOption == 1:
-            firstRow = int(input("1st row to swap: "))
-            secondRow = int(input("2nd row to swap: "))
-            rowOperations.interchange(matrix, firstRow, secondRow)
-            print(matrix)
-        elif userOption == 2:
-            pass
-        elif userOption == 3:
-            pass
-        else:
-            raise ValueError
-    except ValueError:
-        print("Invalid option!")
-
-
-
-def reducedEchelon(matrix):
-    print("test")
+    def reducedEchelon(matrix):
+        print("test")
 
 def main():
-
     userMatrix = []
-
     print("---------------------------------------------------------------------------------")
     print("-------------------Linear Alebra Calculator: Row Reduction ----------------------")
     print("---------------------------------------------------------------------------------")
@@ -86,7 +81,13 @@ def main():
     print("Elements are entered from row to row...")
     print("\n")
 
-    generateMatrix(numCols, numRows, userMatrix)
+    matrix = base.generateMatrix(numRows, numCols, userMatrix)
+
+    print("\n")
+    print("Here is your generated matrix...")
+    print("\n")
+
+    base.printGivenMatrix(matrix)
 
     print("Would you like your array in Regular Echelon Form or Reduced Echelon Form?")
     print("------------------------------(EF) or (REF)-------------------------------")
@@ -94,19 +95,13 @@ def main():
     try:
         userInput = input("Enter your choice: ")
         if userInput == "EF":
-            regularEchelon(userMatrix)
+            base.regularEchelon(matrix)
         elif userInput == "REF":
-            reducedEchelon(userMatrix)
+            base.reducedEchelon(matrix)
         else:
             raise ValueError
     except ValueError:
         print("Invalid input :(")
-
-            
-
-    
-
-
 
 
 if __name__ == '__main__':
